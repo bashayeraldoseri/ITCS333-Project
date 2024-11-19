ec67635f5ab7a191676ce06c1e91af5548610a67
araed <ameenaraed221@gmail.com>
2024-11-20 00:14:15 +0300

    Profile Page (Base)

---
+++ navbar.php
@@ -0,0 +1,45 @@
+<div class="container-fluid">
+  <header>
+    <div class="container-fluid p-2">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">UOB Booking System</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="me-auto"></div>
+            <ul class="nav nav-tabs">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="templates/profile.html">Profile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="templates/dashboard.html">Dashboard</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="templates/login.html">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="templates/register.html">Register</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+</div>
