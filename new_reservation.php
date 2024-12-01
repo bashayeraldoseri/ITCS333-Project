1228f7c7ad18076800f8d0d0bbdc89e39b4116e3
bashayeraldoseri <bashayeraldoseri421@gmail.com>
2024-12-01 22:19:42 +0300

    Update new_reservation.php

--- new_reservation.php
+++ new_reservation.php
@@ -35,30 +35,39 @@
             $Start_Time = $_POST['start_time'];
             $Duration = $_POST['duration'];
             $End_Time = $_POST['start_time'] + $_POST['duration']; //not sure 
+            $capacity = $_POST['capacity'];
 
 
 
-            // Validate input (basic example)
+
+            // Validate input 
             // Check capacity
                 $query = "SELECT capacity FROM rooms WHERE Room_ID = ?";
                 $stmt = $pdo->prepare($query);
                 $stmt->execute([$room_id]);
                 $roomData = $stmt->fetch();
-                $capacity = $roomData['capacity'];
 
+                if ($capacity < 5) {
+                    $error_message_capacity_less_than = "The minimum number that the university allows to request a room reservation is 5 ! ";
+                }
 
+                if ($roomData && $capacity > $roomData['capacity']){
+                    $error_message_capacity_exceeds = "Capacity exceeds the room's capacity of ! " . $roomData['capacity'];
+                }
 
+                if ($roomData && $capacity <= $roomData['capacity']) {
 
+                    //another condition about time and date
+                    // Insert booking
 
-
-
-            // Prepare and execute the SQL query
-            $stmt = $pdo->prepare("INSERT INTO bookings (user_ID, Status, Title, Start_Date, End_Date, Duration, End_Time) VALUES 
-            (?, ?, ?, ?, ?, ?, ?)");
-            $stmt->execute([$id, $Status, $Title, $Start_Date, $End_Date, $Start_Time, $Duration, $End_Time]);
-            
-            // Successful insertion
-            header('Location: index.php'); // Redirect to items list page
+                    // Prepare and execute the SQL query
+                    $stmt = $pdo->prepare("INSERT INTO bookings (user_ID, Status, Title, Start_Date, End_Date, Duration, End_Time) VALUES 
+                    (?, ?, ?, ?, ?, ?, ?)");
+                    $stmt->execute([$id, $Status, $Title, $Start_Date, $End_Date, $Start_Time, $Duration, $End_Time]);
+                    
+                    // Successful insertion
+                    header('Location: index.php'); // Redirect to items list page
+                        }
 
 
             
@@ -85,15 +94,24 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <script>
+
+    // control the formated of date in html to accurate the sql formated        
     $( function() {
-        //your code is here
-        
+
         $( "#datepicker1" ).datepicker({dateFormat: "yy-mm-dd"});
         $( "#datepicker2" ).datepicker({dateFormat: "yy-mm-dd"});
 
-
-
     } );
+
+    // control the length of capacity 
+    function maxLengthCheck(object){
+        
+        if (object.value.length > object.maxLength)
+       object.value = object.value.slice(0, object.maxLength)
+
+        }
+
+
     </script>
 
     </head>
@@ -127,34 +145,40 @@
             <div class="form-group">
             <label for="start_time">Start Time of the event:</label>
             <select name="start_time" class="form-control" id="start_time">
-            <option value="1">1am</option>
-            <option value="2">2am</option>
-            <option value="3">3am</option>
-            <option value="4">4am</option>
-            <option value="5">5am</option>
+            <option value="8">8 am</option>
+            <option value="9">9 am</option>
+            <option value="10">10 am</option>
+            <option value="11">11 am</option>
+            <option value="12">12 pm</option>
+            <option value="13">13 pm</option>
+            <option value="14">14 pm</option>
+            <option value="15">15 pm</option>
+            <option value="16">16 pm</option>
+            <option value="17">17 pm</option>
+            <option value="18">18 pm</option>
             </select>
-            <p>*The time allowed to reserve based on university times <br>*University times start from 8am to 18pm <br> *Not allowed to reserve at 19pm or after
+            <p>*The time allowed to reserve is based on university times <br>*University times start from 8 am to 18 pm <br> *Not allowed to reserve at 19 pm or after
             </p>
             </div>
 
-            <!--assuming each hour = 50 min-->
+            
             <div class="form-group">
             <label for="duration">Duration of the event:</label>
             <select name="duration" class="form-control" id="duration">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
+            <option value="1">1 hour</option>
+            <option value="2">2 hours</option>
+            <option value="3">3 hours</option>
+            <option value="4">4 hours</option>
+            <option value="5">5 hours</option>
             </select>
-            <p>*Duration of the event that take  <br> *The university allowed 1 to 5 maximum hours duration for one event</p>
+            <p>*Duration of the event that takes  <br> *The university allows 1 to 5 maximum hours duration for one event</p>
             </div>
 
-           
 
             <div class="form-group">
                 <label for="capacity">Capacity:</label>
-                <input type="number" class="form-control" name="capacity"  id="capacity"  min="5" max="<?php $capacity; ?>" >
+                <input type="number" oninput="maxLengthCheck(this)" class="form-control" name="capacity"  id="capacity"  min="5" max = "120" maxlength = "3" >
+                <p>*The minimum number that the university allows to request a room reservation is 5</p>
             </div>
 
             <button type="submit" name='submit' class="btn btn-primary">Submit Reservation</button>
@@ -163,7 +187,8 @@
         
             <p style="color: red;">
             <?php if (isset($error_message)) echo $error_message; ?>
-            </p>
+            <?php if (isset($error_message_capacity_less_than)) echo $error_message_capacity_less_than; ?>
+            <?php if (isset($error_message_capacity_exceeds)) echo $error_message_capacity_exceeds; ?> 
             
         </form>
         <p><a href="index.php">Back to Room Browsing</a></p>
