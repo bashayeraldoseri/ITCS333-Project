8ee2e73a890c6125b0d3e64c7ef19f1c285d4ed8
FATEMA AL SWAR <202205785@stu.uob.edu.bh>
2024-12-10 01:50:45 +0300

    <background changes>

--- AboutUs.php
+++ AboutUs.php
@@ -20,7 +20,12 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == 1) {
   <link rel="stylesheet" href="css/styles.css" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-  <style>
+  <style> 
+
+  body{
+    background-color: #EFF1F3;
+  } 
+  
     footer {
       text-align: center;
       background-color: #a0b8cf;
